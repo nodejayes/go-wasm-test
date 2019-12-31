938d2f19cc1e1747979d01035e4b9ce5865c586b
Markus <markusgilg@outlook.de>
2019-12-31 14:10:17 +0100

    init

---
+++ cmd/server.go
@@ -0,0 +1,19 @@
+// The Implementation of a Simple File Server
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	listen = flag.String("listen", ":8080", "listen address")
+	dir    = flag.String("dir", "./public", "directory to serve")
+)
+
+func main() {
+	flag.Parse()
+	log.Printf("server running on %q", *listen)
+	log.Fatal(http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir))))
+}
